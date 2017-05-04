559d76a4e627b3d5be4f746d6a4b04d0263921ec
Irfan Hazim Bin Azhar <irfanhazim96.ih@gmail.com>
2017-05-04 20:45:36 +0800

    Update BinaryToString.java

--- BinaryToString.java
+++ BinaryToString.java
@@ -11,21 +11,21 @@ public class BinToString
 
 		//declareFileWriter
 		PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("text.txt")));
-		PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("reversetext.txt")));
+		PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("reversetext.txt"))); //for reverse
 
 		new Scanner = new Scanner(bin);
 		String words = scanner.nextLine();
 		
-		String[] code = words.split(" ");
+		String[] sentence = words.split(" ");
  		String str = "";
 		
-		for(int x = ; x < code.length; x++)
+		for(int x = ; x < sentence.length; x++)
 		{
-			str += (char)Integer.parseInt(code[x],2);
+			str += (char)Integer.parseInt(sentence[x],2);
 		}
 
 		System.out.println("Binary to String. Done.");
-		System.out.println("Binary to String. Success.");
+		System.out.println("Binary to String. Successful.");
 
  		String reverse = new StringBuffer(str).reverse().toString();
  		
